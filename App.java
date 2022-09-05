ec48dd7a3e4bee334e3ea744cc9b0ce38a5b30c3
Orosz Levente <oroszleventee1403@gmail.com>
2022-09-05 09:31:46 +0200

    Legel

---
+++ App.java
@@ -0,0 +1,10 @@
+import java.net.PortUnreachableException;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Interfész");
+
+        Pablo kecske = new Pablo();
+        kecske.legel();
+    }
+}
